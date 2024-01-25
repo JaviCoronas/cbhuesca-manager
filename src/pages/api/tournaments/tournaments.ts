c79c2b0f46541ca6e70ab9bb911c712ec2091a2b
Javi Coronas <javier.coronas@externos.tinsa.com>
2024-01-25 17:57:35 +0100

    main: obtain calendar from supabase

---
+++ src/pages/api/tournaments/tournaments.ts
@@ -0,0 +1,23 @@
+import { supabase } from "../../../lib/supabase";
+
+export interface Tournament {
+  id: string;
+  date: string;
+  name: string;
+  inscription: string;
+  draw: string;
+}
+
+export async function getAllTournaments(): Promise<Tournament[] | null> {
+  const { data, error } = await supabase
+    .from("tournaments")
+    .select("*")
+    .order("date", { ascending: false });
+
+  if (error) {
+    console.error("Error obteniendo los datos", error);
+    return;
+  }
+
+  return data as Tournament[];
+}
