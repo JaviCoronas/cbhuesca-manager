8a9039bb3bd653aa23accb132addb70550a7d1d3
Javi Coronas <javier.coronas@externos.tinsa.com>
2024-02-09 14:44:34 +0100

    main: including adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
 import react from "@astrojs/react";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  integrations: [tailwind(), mdx(), react()]
+  integrations: [tailwind(), mdx(), react()],
+  adapter: node({
+    mode: "standalone"
+  })
 });
