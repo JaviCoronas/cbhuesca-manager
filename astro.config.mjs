3419f076e3bd482e770ac09bfa2902381cf86f15
Javi Coronas <javier.coronas@externos.tinsa.com>
2024-02-13 16:39:43 +0100

    main: netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,16 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import netlify from '@astrojs/netlify';
 
 import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
+  output: 'server',
   integrations: [tailwind(), mdx(), react()],
   adapter: node({
     mode: "standalone"
-  })
+  }),
+  adapter: netlify()
 });
