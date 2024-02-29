79cbf423a33c3c19a670ba8cd6657ae412aa4d75
Javi Coronas <javier.coronas@externos.tinsa.com>
2024-02-29 10:42:39 +0100

    main: first version last numbers

---
+++ src/components/archivements/Archievements.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import CardArchivement from "./CardArchivement";
+
+const Archivements: React.FC = () => {
+  const cardData = [
+    {
+      title: "Socios",
+      text: "Todos somos parte de la familia",
+      colour: "#7209b7",
+      number: 158,
+    },
+    {
+      title: "Medallas en Campeonatos de España",
+      text: "Grandes logros de nuestros jugadores",
+      colour: "#4361ee",
+      number: 34,
+    },
+    {
+      title: "Participaciones Selección Nacional",
+      text: "Aportaciones y resultados en comepticiones internacionales",
+      colour: "#3a0ca3",
+      number: 86,
+    },
+    {
+      title: "Entrenadores titulados",
+      text: "4 entrenadores Nivel III, 2 entrenadores Nivel II, 6 de nivel ShuttleTime",
+      colour: "#f72585",
+      number: 12,
+    },
+  ];
+
+  return (
+    <div className="flex flex-col justify-center items-center md:w-3/4 mb-10">
+      <div className="w-full">
+        <h2 className="font-sans text-4xl mb-2 mt-3">
+          <strong className="text-5xl font-sans">NUESTROS</strong> NÚMEROS
+        </h2>
+        <CardArchivement data={cardData} />
+      </div>
+    </div>
+  );
+};
+
+export default Archivements;
---
+++ src/components/archivements/CardArchivement.tsx
@@ -0,0 +1,100 @@
+import React, { useEffect, useRef } from "react";
+import "./CardCounter.css";
+
+interface CardData {
+  title: string;
+  number: number;
+  text: string;
+  colour: string;
+}
+
+const CardArchivement: React.FC<{ data: CardData[] }> = ({ data }) => {
+  const countersRef = useRef<
+    { counterElement: HTMLDivElement; targetNumber: number }[]
+  >([]);
+
+  useEffect(() => {
+    const observerOptions: IntersectionObserverInit = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.5, // Adjust this threshold as needed
+    };
+
+    const observer = new IntersectionObserver(
+      handleIntersection,
+      observerOptions
+    );
+
+    countersRef.current.forEach(({ counterElement }) => {
+      observer.observe(counterElement);
+    });
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  const handleIntersection: IntersectionObserverCallback = (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const targetIndex = countersRef.current.findIndex(
+          (item) => item.counterElement === entry.target
+        );
+
+        if (targetIndex !== -1) {
+          animateCounter(
+            countersRef.current[targetIndex].counterElement,
+            countersRef.current[targetIndex].targetNumber
+          );
+        }
+      }
+    });
+  };
+
+  const animateCounter = (
+    counterElement: HTMLDivElement,
+    targetNumber: number
+  ) => {
+    let currentNumber = 1;
+
+    const updateCounter = () => {
+      counterElement.textContent = currentNumber.toString();
+
+      if (currentNumber < targetNumber) {
+        currentNumber++;
+        requestAnimationFrame(updateCounter);
+      }
+    };
+
+    updateCounter();
+  };
+
+  return (
+    <div className="flex md:flex-row flex-col justify-center items-center text-white">
+      {data.map((item, index) => (
+        <div
+          className={`m-2 shadow-xl bg-[${item.colour}] flex flex-col h-96 md:w-64 w-72 justify-center items-center rounded-xl text-center`}
+          key={index}
+        >
+          <h2 className="flex top-0 mt-3 h-44 font-bold text-xl">
+            {item.title}
+          </h2>
+          <p>{item.text}</p>
+          <div
+            className="flex text-6xl h-full justify-center items-center"
+            ref={(el) =>
+              (countersRef.current[index] = {
+                counterElement: el!,
+                targetNumber: item.number,
+              })
+            }
+          >
+            p
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CardArchivement;
